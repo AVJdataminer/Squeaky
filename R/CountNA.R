9a7fda3b53c1f7672c519273f5098cc6e4e53a3d
Aiden V Johnson <30507129+AVJdataminer@users.noreply.github.com>
2017-09-12 06:40:52 -0400

    Update CountNA.R

--- R/CountNA.R
+++ R/CountNA.R
@@ -7,7 +7,7 @@ CountNA<-function(df){
   out=df%>%
     map_df(function(x) sum(is.na(x)))%>%
     gather(feature,num_nulls)%>%
-    num_nulls>0 >%
+    num_nulls>0 %>%
     print(n=50)
   return(out)
 }
