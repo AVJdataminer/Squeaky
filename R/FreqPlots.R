266c56d62c456bbb24d1ac1d9cb56fd14b8b7ca5
Aiden V Johnson <30507129+AVJdataminer@users.noreply.github.com>
2017-11-01 14:57:22 -0400

    Added qualitative and quantitative data split  for input dataframe

--- R/FreqPlots.R
+++ R/FreqPlots.R
@@ -3,13 +3,31 @@
 #code for factor variable bins.
 
 #FreqPlots(df,folder)
-ds=PCAmixdata::splitmix(df)
 #define:
 #response=response
 #theme_new
-# require(ggplot2)
+require(ggplot2)
+theme_new <- function(base_size = 12, base_family = "Helvetica"){
+  theme_bw(base_size = base_size, base_family = base_family) %+replace%
+    theme(
+      #line = element_line(colour="black"),
+      #text = element_text(colour="black"),
+      axis.title = element_text(size = 14),
+      #axis.text = element_text(colour="black", size=8),
+      strip.text = element_text(size=12,colour="darkred"),
+      legend.key=element_rect(colour=NA, fill =NA),
+      panel.grid = element_blank(),   
+      panel.border = element_rect(fill = NA, colour = "darkred", size=1),
+      panel.background = element_rect(fill = "white", colour = "white"), 
+      strip.background = element_rect(fill = "gray", colour="white")
+    )
+}
  FreqPlots<-function(df, folder){
    require(ggplot2)
+   ds=PCAmixdata::splitmix(df)
+   dfn<- data.frame(ds$X.quanti)
+   dff=data.frame(ds$X.quanti$response,ds$X.quali)
+   names(dff)[1]<-"response"
    plotFunc <- function(x, na.rm = TRUE){
     nm <- names(x)
      for (i in seq_along(nm)) {
@@ -19,7 +37,17 @@ ds=PCAmixdata::splitmix(df)
          theme(axis.text.x = element_text(angle = 0, hjust = 1))
        ggsave(filename=paste(folder,"/",nm[i],".png", sep=""),plot=plots)
      }}
-  plotFunc(df)
+  plotFunc(dfn)
+ 
+   plotFunc <- function(x, na.rm = TRUE){
+     nm <- names(x)
+     for (i in seq_along(nm)) {
+       plots <-ggplot(x,aes_string(x = nm[i]), fill=response) +facet_grid(~response)+
+         geom_histogram(stat="count",fill="darkred", color="lightgrey", position="dodge")+theme_new()+
+         theme(legend.position = "none")+ ggtitle(paste(nm[i]))+
+         theme(axis.text.x = element_text(angle = 0, hjust = 1))
+       ggsave(filename=paste(folder,"/",nm[i],".png", sep=""),plot=plots)
+     }}
+   plotFunc(dff)
  }
- FreqPlots(ds$X.quali,getwd())
  
