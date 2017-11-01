b58acb6436480c7c1c01f7b976ee4832539f8d46
Aiden V Johnson <30507129+AVJdataminer@users.noreply.github.com>
2017-11-01 14:31:28 -0400

    Update FreqPlots.R

--- R/FreqPlots.R
+++ R/FreqPlots.R
@@ -3,17 +3,23 @@
 #code for factor variable bins.
 
 #FreqPlots(df,folder)
-
-FreqPlots<-function(df, folder){
-require(ggplot2)
- plotFunc <- function(x, na.rm = TRUE){
-  nm <- names(x)
-  for (i in seq_along(nm)) {
-    plots <-ggplot(x,aes_string(x = nm[i]), fill="lightblue") +
-      geom_bar(stat="count",position = position_dodge())+theme_bw()+
-      theme(legend.position = "none")+ ggtitle(paste(nm[i]))+ 
-      theme(axis.text.x = element_text(angle = 45, hjust = 1))
-    ggsave(filename=paste(folder,"/Freq_plot_",nm[i],".png", sep=""),plot=plots)
-  }}
- plotFunc(df)
-} 
+ds=PCAmixdata::splitmix(df)
+#define:
+#response=response
+#theme_new
+# require(ggplot2)
+ FreqPlots<-function(df, folder){
+   require(ggplot2)
+   plotFunc <- function(x, na.rm = TRUE){
+    nm <- names(x)
+     for (i in seq_along(nm)) {
+       plots <-ggplot(x,aes_string(x = nm[i]), fill=response) +facet_grid(~response)+
+        geom_histogram(fill="darkred", color="lightgrey", position="dodge")+theme_new()+
+         theme(legend.position = "none")+ ggtitle(paste(nm[i]))+
+         theme(axis.text.x = element_text(angle = 0, hjust = 1))
+       ggsave(filename=paste(folder,"/",nm[i],".png", sep=""),plot=plots)
+     }}
+  plotFunc(df)
+ }
+ FreqPlots(ds$X.quali,getwd())
+ 
